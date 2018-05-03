b38e1d40afd04aafeaae74df855e3cd0c960ee52
JoseManuelGC <josema_10_1996@hotmail.com>
2018-05-03 20:12:32 +0200

    friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412111119) do
+ActiveRecord::Schema.define(version: 20180503180448) do
 
   create_table "collections", force: :cascade do |t|
     t.string "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.boolean "confirmed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "notes", force: :cascade do |t|
