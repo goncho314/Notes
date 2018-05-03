39bf626c9177cb4c1c4bfaef8812cff2e5b09d79
goncho314 <goncho314@gmail.com>
2018-05-03 20:02:21 +0200

    Migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get "logout" => "session#destroy", :as => "logout"
+  get "login" => "session#new", :as => "login"
+  post "login" => "session#create"
+  get "signup" => "users#new", :as => "signup"
+
+  resources :collections
+  resources :notes
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "notes#index"
 end
